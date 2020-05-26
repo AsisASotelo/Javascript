1e7ac8d05e04e7e8a51cea8977e09a77be923838
AsisASotelo <asis.a.sotelo@gmail.com>
2020-05-25 20:31:08 -0700

    Add React-Native functionality

--- React-Native/confusion/App.js
+++ React-Native/confusion/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native'; // These are from the react-native library
+import Main from './components/MainComponent';
+// import { StyleSheet, Text, View } from 'react-native'; // These are from the react-native library
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Changes you make will automatically be reloaded.</Text>
-    </View>
+        <Main/>
+
+
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <Text>What is love but just a feeling I is a great feeling Itis what everyone cares about.</Text>
+    // </View>
   );
 }
 
 //Inline javascript styles, unlike css script
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#0ff',
+//     alignItems: 'center', // This aligns items vertically
+//     justifyContent: 'center', //This aligns items horizontally
+//   },
+// });
---
+++ React-Native/confusion/components/MenuComponent.js
@@ -0,0 +1,35 @@
+import React,{Component} from 'react';
+import { View,FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
+
+function Menu(props) {
+    // The renderItem {item} parameter takes whatever data in the data param
+    // props.dishes array and supplies each item to the renderMenuItem
+    // The renderItem {index} parameter takes whatever data in the keyExtractor
+    // param, which is supposed to be a string
+
+    const renderMenuItem = ({item,index}) =>{
+        return(
+            <ListItem 
+                key={index} 
+                title = {item.name}
+                subtitle = {item.description}
+                hideChevron={true}
+                leftAvatar = {{ source: require('./images/uthappizza.png')}} // This is javascript item supplied as an object, the require should be in the code, when you say require it has to be an EXACT image name.
+                />
+
+
+        );
+    }
+    return(
+        <FlatList  
+            data = {props.dishes}
+            renderItem = {renderMenuItem}
+            keyExtractor = {item => item.id.toString()}
+            />
+
+    );
+
+}
+
+export default Menu;
